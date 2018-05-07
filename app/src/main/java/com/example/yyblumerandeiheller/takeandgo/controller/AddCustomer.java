63a7b26ea4785a568b0e1bceac5508c54047c71e
Heller <eellhhaannaann@gmail.com>
2018-05-07 18:03:50 +0300

    תיקון באג בהעלאת נתונים בתהליך הוספת costumer

--- app/src/main/java/com/example/yyblumerandeiheller/takeandgo/controller/AddCustomer.java
+++ app/src/main/java/com/example/yyblumerandeiheller/takeandgo/controller/AddCustomer.java
@@ -1,17 +1,21 @@
 package com.example.yyblumerandeiheller.takeandgo.controller;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
-
 import com.example.yyblumerandeiheller.takeandgo.R;
-import com.example.yyblumerandeiheller.takeandgo.model.datasource.ListsDataSource;
+import com.example.yyblumerandeiheller.takeandgo.model.backend.DataSource;
+import com.example.yyblumerandeiheller.takeandgo.model.backend.FactoryMethod;
 import com.example.yyblumerandeiheller.takeandgo.model.entities.Customer;
 
+
+
     public class AddCustomer extends AppCompatActivity {
-        ListsDataSource listsDataSource = new ListsDataSource();
+
+        DataSource ds;
+       // ListsDataSource listsDataSource = new ListsDataSource();
         EditText id,firstName,lastName, phoneNum, email, creditCardNum;
 
         @Override
@@ -19,9 +23,14 @@ import com.example.yyblumerandeiheller.takeandgo.model.entities.Customer;
             super.onCreate( savedInstanceState );
             setContentView( R.layout.activity_add_customer );
 
+            ds = FactoryMethod.getDataSource();
+
             id = ((EditText) findViewById( R.id.id ));
             firstName = ((EditText) findViewById( R.id.firstName ));
             lastName = ((EditText) findViewById( R.id.lastName ));
+            phoneNum = ((EditText) findViewById( R.id.phoneNum ));
+            email = ((EditText) findViewById( R.id.email ));
+            creditCardNum = ((EditText) findViewById( R.id.creditCardNum ));
 
         }
 
@@ -29,8 +38,12 @@ import com.example.yyblumerandeiheller.takeandgo.model.entities.Customer;
             Customer customer = new Customer(
                     id.getText().toString(),
                     firstName.getText().toString(),
-                    lastName.getText().toString(),phoneNum.toString(), email.toString(), creditCardNum.toString());
+                    lastName.getText().toString(),
+                    phoneNum.getText().toString(),
+                    email.getText().toString(),
+                    creditCardNum.getText().toString());
 
-            listsDataSource.addCustomer( customer );
+           // listsDataSource.addCustomer( customer );
+            ds.addCustomer( customer );
         }
 }
--- app/src/main/java/com/example/yyblumerandeiheller/takeandgo/controller/MainActivity.java
+++ app/src/main/java/com/example/yyblumerandeiheller/takeandgo/controller/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.yyblumerandeiheller.takeandgo.controller;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+
 import com.example.yyblumerandeiheller.takeandgo.R;
+import com.example.yyblumerandeiheller.takeandgo.model.backend.*;
+import com.example.yyblumerandeiheller.takeandgo.model.entities.Customer;
 import com.example.yyblumerandeiheller.takeandgo.model.utils.ConstantsAndEnums;
 
 
+
+
+
     public class MainActivity extends AppCompatActivity {
+
+        DataSource ds;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            ds = FactoryMethod.getDataSource();
+ds.addCustomer(new Customer("xcxx","cxxc","ffff","cxcxcx","cxcxc", "ccxcxcx"));
         }
 
         public void addCustomerBtnClick(View view) {
